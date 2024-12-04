58da8d4b17b9da12ae8eaf6bcc73585fd61c1a7a
Ramonsouzadasilva <ramon.souzadasilva2@gmail.com>
2024-12-04 14:06:03 -0300

    Atualizando o código para o atual

--- backend/api/src/mesa/rotas/rotas-mesa.php
+++ backend/api/src/mesa/rotas/rotas-mesa.php
@@ -5,11 +5,6 @@ function criarRotasMesa($app, PDO $pdo)
 {
     $mesaController = new MesaController($pdo);
 
-    // Rota principal
-    $app->get('/', function ($req, $res) {
-        $res->send('Bem-vindo ao sistema de Reservas!');
-    });
-
     $app->get('/mesas', function ($req, $res) use ($mesaController) {
         $mesaController->listarMesas($req, $res);
     });
